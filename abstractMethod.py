0eae5a9e437162604c7546712aa1580ad6c7255f
LeHanhJ <101995252+LeHanhJ@users.noreply.github.com>
2022-08-01 19:54:35 -0700

    created abstraction assignment

---
+++ abstractMethod.py
@@ -0,0 +1,16 @@
+from abc import ABC, abstractmethod
+
+class Water(ABC):
+    def ouncesTotal(self, amount):
+        print("The total amount of water you have in your bottle is " + amount +" ounces today.")
+    @abstractmethod
+    def ouncesDrank(self,amount):
+        pass
+
+class waterDrank(Water):
+    def ouncesDrank(self, amount):
+        print("You've consumed {} ounces of water so far.".format(amount))
+
+obj = waterDrank()
+obj.ouncesTotal("48")
+obj.ouncesDrank("32")
