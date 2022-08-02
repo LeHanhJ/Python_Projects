2b7027c6eafa14b0e46fc7c0df2348ac527c75d9
LeHanhJ <101995252+LeHanhJ@users.noreply.github.com>
2022-08-01 19:40:04 -0700

    added comments to code

--- protectedandPrivate.py
+++ protectedandPrivate.py
@@ -1,25 +1,25 @@
-class Protected:
-    def __init__(self):
-        self._protectedVar = 0
+class Protected: #creates class Protected
+    def __init__(self): #defines function within itself
+        self._protectedVar = 0 #assigns 0 to the protectedVar in function Protected()
 
-obj = Protected()
-obj._protectedVar = 28
-print(obj._protectedVar)
-
-
+obj = Protected() #Sets var obj as Protected()
+obj._protectedVar = 28 #assigns obj the number 28 as the variable
+print(obj._protectedVar) #prints assigned number from previous line
 
 
-class Private:
-    def __init__(self):
-        self.__privateVar = 2
 
-    def getPrivate(self):
-        print(self.__privateVar)
 
-    def setPrivate(self,private):
-        self.__privateVar = private
+class Private: #create class Private
+    def __init__(self): #defines function within itself
+        self.__privateVar = 2 #sets __privateVar to be 2 initially
 
-obj = Private()
-obj.getPrivate()
-obj.setPrivate(99)
-obj.getPrivate()
+    def getPrivate(self): #defines getPrivate function
+        print(self.__privateVar) #prints __privateVar, which was previously defined as 2
+
+    def setPrivate(self,private): #defines setPrivate function, adds "private" to make sure we call upon it when we change the variable
+        self.__privateVar = private #uses "private" as assigned in input setPrivate() function
+
+obj = Private() #calls Private() function, which is initially 2
+obj.getPrivate() #prints out __privateVar variable, which as said before is 2
+obj.setPrivate(99) #sets setPrivate() to 99 by giving "private" argument
+obj.getPrivate() #updates getPrivate function with update
